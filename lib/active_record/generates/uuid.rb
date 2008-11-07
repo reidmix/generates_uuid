902534c384c257eaffba9c68f8f9acba36086c03
Reid MacDonald <reid@laruby.com>
2008-11-07 18:32:36 +0000

    Kernel is not needed for accessing objects with no namespace
    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/ruby/plugins/generates_uuid/trunk@13589 a06862d0-bf29-e17f-e302-fbc15f989f17

--- lib/active_record/generates/uuid.rb
+++ lib/active_record/generates/uuid.rb
@@ -13,8 +13,8 @@ module ActiveRecord
         def generates_uuid(field = :uuid)
           validates_uniqueness_of field 
           before_validation_on_create do |o|
-            # Kernel::UUID is uuidtools, otherwise there's a namespace collision with this module
-            o.send("#{ field }=", Kernel::UUID.timestamp_create.to_s)
+            # ::UUID is uuidtools, otherwise there's a namespace collision with this module
+            o.send("#{ field }=", ::UUID.timestamp_create.to_s)
           end
         end
       end    
